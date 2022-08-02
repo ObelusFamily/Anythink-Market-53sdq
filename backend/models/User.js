23bf942df54d462de73fd7139f5902fe8f5a5d09
cnu1812 <cnu1812@gmail.com>
2022-08-02 12:53:45 +0530

    bug fixed

--- backend/models/User.js
+++ backend/models/User.js
@@ -74,7 +74,7 @@ UserSchema.methods.toAuthJSON = function() {
     email: this.email,
     token: this.generateJWT(),
     bio: this.bio,
-    image: this.image || 'placeholder.png',
+    image: this.image || "placeholder.png",
     role: this.role
   };
 };
